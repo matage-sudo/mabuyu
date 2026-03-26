e8b74f19326c4a3327c6ff19f3419bbf29dfd413
matage-sudo <bonifacematage31@gmail.com>
2026-03-26 07:01:59 -0400

    Initial commit with website files

---
+++ script.js
@@ -0,0 +1,7 @@
+// Floating WhatsApp Button
+const btn = document.createElement("a");
+btn.href = "https://wa.me/254793400696";
+btn.innerText = "💬";
+btn.className = "float-btn";
+
+document.body.appendChild(btn);
